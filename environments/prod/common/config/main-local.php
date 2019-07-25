66af8100bbe3893c10fc8a4be12ef450ccd9733a
xstnet <792539542@qq.com>
2019-07-26 00:07:05 +0800

    更新配置

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
 		'db' => [
 			'class' => 'yii\db\Connection',
-			'dsn' => 'mysql:host=127.0.0.1;dbname=yii-admin',
-			'username' => 'root',
-			'password' => 'root',
+			'dsn' => 'mysql:host=127.0.0.1;dbname=xstnet',
+			'username' => 'xstnet_blog',
+			'password' => 'asd#%3GNldjgeHFVv(*&',
 			'charset' => 'utf8',
 			'tablePrefix' => 'x_',
 		],
