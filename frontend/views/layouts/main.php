bda32675ff44570fb883c1d3aafed7110f40135b
xstnet <792539542@qq.com>
2019-08-11 22:29:40 +0800

    update style

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -21,7 +21,7 @@ if (!isset($this->params['keyword'])) {
 if (!isset($this->params['description'])) {
 	$this->params['description'] = '徐善通的技术笔记,分享一些php问题的解决方案';
 }
-$assetVersion = '1.0011';
+$assetVersion = '1.0012';
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
