37c0cae748eb9ac8b4ed8737cf4bc644349885dc
醉丶春风、 <792539542@qq.com>
2020-11-22 00:24:04 +0800

    fixbugs

--- frontend/views/note/index.php
+++ frontend/views/note/index.php
@@ -11,7 +11,7 @@ if (isset($active_menu)) {
 //?>
 <div class="col-md-9 content-left">
 	<div class="list-item">
-        <h4><a target="_blank" href="/columns/linux-note">Linux 学习笔记</a></h4>
+        <h4><a target="_blank" href="/columns/linux-notes">Linux 学习笔记</a></h4>
         <div class="list-item-description">Linux 学习笔记, 基于gitbook搭建的平台</div>
         <div class="list-item-separator"></div>
     </div>
